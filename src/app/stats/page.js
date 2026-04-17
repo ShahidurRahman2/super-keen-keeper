4bfd7bd93bec9d8774b738abd9232cb2804a3c0d
ShahidurRahman2 <imshahidur@gmail.com>
2026-04-17 21:14:18 +0600

    statistic file is completed successfullay

--- src/app/stats/page.js
+++ src/app/stats/page.js
@@ -1,11 +1,63 @@
+"use client";
 
-const Stats = () => {
+import { useGlobal } from "@/context/GlobalContext";
+import {
+    PieChart,
+    Pie,
+    Cell,
+    Tooltip,
+    Legend,
+    ResponsiveContainer,
+} from "recharts";
+
+export default function StatsPage() {
+    const { timeline } = useGlobal();
+
+    // COUNT TYPES  HERE LOOK
+    const callCount = timeline.filter((t) => t.type === "Call").length;
+    const textCount = timeline.filter((t) => t.type === "Text").length;
+    const videoCount = timeline.filter((t) => t.type === "Video").length;
+
+    // CHART DATA  HERE LOOK
+    const data = [
+        { name: "Call", value: callCount },
+        { name: "Text", value: textCount },
+        { name: "Video", value: videoCount },
+    ];
+
+    const COLORS = ["#3b82f6", "#10b981", "#f59e0b"];
+
     return (
-        <div>
+        <div className="p-6">
+            <h1 className="text-2xl font-bold mb-6">
+                Friendship Analytics
+            </h1>
 
-            <h1 className="text-center mt-10 text-2xl">Stats Page</h1>
+            {timeline.length === 0 ? (
+                <p>No data yet</p>
+            ) : (
+                <div className="w-full h-[400px]">
+                    <ResponsiveContainer>
+                        <PieChart>
+                            <Pie
+                                data={data}
+                                cx="50%"
+                                cy="50%"
+                                outerRadius={120}
+                                dataKey="value"
+                                label
+                            >
+                                {data.map((entry, index) => (
+                                    <Cell key={index} fill={COLORS[index]} />
+                                ))}
+                            </Pie>
+
+                            <Tooltip />
+                            <Legend />
+                        </PieChart>
+                    </ResponsiveContainer>
+                </div>
+            )}
         </div>
     );
-};
-
-export default Stats;
+}
